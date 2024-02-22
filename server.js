8083884d204cf698caa02070c6af510ce9f58062
Manish Gupta <guptamns3786@gmail.com>
2024-02-22 13:00:11 +0530

    my second commit

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: [
+          "http://localhost:8080",
+          "http://localhost:3000",
+          "http://18.234.53.21", // react localhost mobile ip
+          "http://3.109.156.217",
+          "https://test.rentofurniture.com"
+        ],
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true,
+      }
+))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
